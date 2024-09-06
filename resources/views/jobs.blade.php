105934867149fb7f205f22b5e073d1be1f06d6ba
jkimble <jkimble.dev@gmail.com>
2024-09-06 16:17:02 -0400

    finished eloquent lessons

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -5,11 +5,15 @@
     <div class="space-y-4">
         @foreach($jobs as $job)
             <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200">
-                <div>{{ $job->employer->name }}</div>
+                <div class="font-bold">{{ $job->employer->name }}</div>
                 <div>
-                    <strong>{{ $job['title'] }}</strong>: Salary {{ $job['salary'] }}
+                    <strong>{{ $job->title }}</strong>: Salary {{ $job->salary }}
                 </div>
             </a>
         @endforeach
+
+        <div>
+            {{ $jobs->links() }}
+        </div>
     </div>
 </x-layout>
