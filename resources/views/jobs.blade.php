a67f58c54a6f96a5990e050c4d910bc38dd34361
jkimble <jkimble.dev@gmail.com>
2024-09-06 14:02:24 -0400

    up to day 13

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,13 +2,14 @@
     <x-slot:heading>
         Job Listings
     </x-slot:heading>
-    <ul>
+    <div class="space-y-4">
         @foreach($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}">
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200">
+                <div>{{ $job->employer->name }}</div>
+                <div>
                     <strong>{{ $job['title'] }}</strong>: Salary {{ $job['salary'] }}
-                </a>
-            </li>
+                </div>
+            </a>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
